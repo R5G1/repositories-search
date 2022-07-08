e48486d3cf245f06d37b23f5995e8848f07ca11f
Vladislav <noreply@github.com>
2022-07-08 13:47:55 +0300

    feat: add MeinAPI.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import './App.scss';
-import ReactForm from './pages/React-forms/forms';
-import React from 'react';
+import MeinAPI from './pages/MeinAPI/MeinAPI';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <ReactForm />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <MeinAPI />
+    </div>
+  );
 }
 
 export default App;
